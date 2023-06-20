4a95c247b2231b53b00832fcffd5da6cbe4ee7be
Lelvinn <ocheni.victor@gmail.com>
2023-06-20 06:06:59 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - A short description
+ *Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'e' || a == 'q')
+
+		continue;
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
